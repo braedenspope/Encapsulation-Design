b4a8add856c98bfcb030448ab90ed1cfffd6fe99
braedenspope <104460343+braedenspope@users.noreply.github.com>
2025-09-17 15:36:52 -0600

    Add files via upload

---
+++ Lab Work/Hello World.cpp
@@ -0,0 +1,21 @@
+/*************************************************************
+ * 1. Name:
+ *      Braeden Pope
+ * 2. Assignment Name:
+ *      Lab 01: Hello World
+ * 3. Assignment Description:
+ *      A simple program to display a message on the screen
+ * 4. What was the hardest part? Be as specific as possible.
+ *      Nothing was super hard about it, the resources given made it simple.
+ * 5. How long did it take for you to complete the assignment?
+ *      30 minutes
+ *****************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World\n";
+    return 0;
+}
